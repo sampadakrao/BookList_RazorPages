32def7b35505b585d0aa63c7e4ec35308d75b94f
Sampada Rao <sampadakrao@gmail.com>
2021-06-26 16:57:52 +0200

    Installed NuGet packages, Configuartion setup, database connection

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext:DbContext
+    {
+     //constructor. (press ctor+tab(twice))
+     //constructor parameter is needed for dependency injection
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        //add book model that was added to the database
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        //ctrl+.
+        //data annotation.'key' will automatically add 'Id' as an identity column
+        [Key]
+        //prop+tab(press twice)
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
